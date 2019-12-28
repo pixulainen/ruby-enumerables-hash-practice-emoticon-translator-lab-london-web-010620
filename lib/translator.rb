8a88a9edaf1c130dce51274fcf329a5756c20bc9
George Apetrei <george.apetrei@gmail.com>
2019-12-28 13:42:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,9 +7,9 @@ def load_library(file_path)
  result = {"get_meaning" => {},"get_emoticon" => {}}
   library.each do |meaning,emoticon|
     result["get_meaning"][emoticon[1]] = meaning
-    result["get_emoticon"][emoticon[0]] =emoticon[1]
-    end
-    result
+    result["get_emoticon"][emoticon[0]] = emoticon[1]
+   end
+   result
 end
 
 def get_japanese_emoticon
